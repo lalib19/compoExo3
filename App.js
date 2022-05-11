de40ddbb4e8c9d6edc75e8ad6aec0b386e8237d0
louis alibert <louis.alib19@gmail.com>
2022-05-11 11:48:09 +0200

    third exercise with image problem

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <View style={styles.container}>   
+    <Image style={styles.image} source={require('./rover.jpg')} />
+    <Text style={styles.text}>Le rover Opportunity</Text>
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+  flex: 1,
+  flexDirection:"row",
+  backgroundColor: '#fff',
+  alignItems: 'center',
+},
+image: {
+  flex: 3,
+  resizeMode: 'contain', 
+  width: "100%",
+  // borderRadius: 15,
+  // borderStyle: "solid",
+  // borderColor: "black",
+  // borderWidth: 1,
+  // borderRightWidth: 1,
+  
+},
+text: {
+  flex: 1,
+  color: "red",
+  fontSize: 20,
+
+}
+
+
+
+
 });
